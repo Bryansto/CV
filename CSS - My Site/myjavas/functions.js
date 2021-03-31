a60d6a59ebe46f0aa31136b1ae08d625952f58a3
Bryansto <51202269+Bryansto@users.noreply.github.com>
2021-03-31 15:45:19 -0400

    Add files via upload

---
+++ CSS - My Site/myjavas/functions.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  var yourname = prompt('What is your name?', 'type name here').toLowerCase();
+  alert('Welcome to my page ' + + yourname.slice(0,1).toUpperCase() + yourname.slice(1,name.length) + '!');
+}
+
+function myFalso() {
+  var yourname = prompt('This is the same but different', 'type name here').toLowerCase();
+  alert('Welcome to my page ' + + yourname.slice(0,1).toUpperCase() + yourname.slice(1,name.length) + '!');
+}
